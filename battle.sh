0057d04a1b045fa553fe107e30f253a4465db136
Zhixiang Li <lizx@seagroup.com>
2018-05-03 00:21:33 +0800

    Support background running program

--- battle.sh
+++ battle.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 BASE_DIR=$(dirname "$0")
 BIN_DIR=${BASE_DIR}/bin
 LOG_DIR=${BASE_DIR}/log
 
-function battle {
-    mkdir -p ${LOG_DIR}
-    java -jar ${BIN_DIR}/gomoku-battle-dashboard-*-jar-with-dependencies.jar -player $1
+function usage {
+    cat <<EOM
+Usage: $(basename "$0") [OPTION]...
+  -c VALUE    specify the configuration file
+  -d          run without UI
+  -h          display help
+EOM
+    exit 2
 }
 
 function main {
-    battle $1
+    local jar_filename="gomoku-battle-dashboard-*-jar-with-dependencies.jar"
+    while getopts ":c:d" opt; do
+        case $opt in
+            c)
+                local player_config=$OPTARG
+                ;;
+            d)
+                jar_filename="gomoku-battle-console-*-jar-with-dependencies.jar"
+                ;;
+            h|*)
+                usage
+                ;;
+        esac
+    done
+    if [ -z "$player_config" ]; then
+        usage
+    fi
+
+    mkdir -p ${LOG_DIR}
+    java -jar ${BIN_DIR}/${jar_filename} -player ${player_config}
 }
 
 main $@
